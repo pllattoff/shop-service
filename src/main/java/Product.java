7e1a6834b0e2b1992c84d97870156f22f0dc705b
Stanislav Platov <pllattoff@gmail.com>
2026-07-16 11:05:30 +0200

    feat: add Product and ProductRepo

---
+++ src/main/java/Product.java
@@ -0,0 +1,9 @@
+import java.math.BigDecimal;
+
+public record Product(Integer id, String name, BigDecimal price) {
+
+    public Product withPrice(BigDecimal price) {
+        return new Product(id, name, price);
+    }
+
+}
